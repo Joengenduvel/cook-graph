293128b410463e0260ebaf8f668b9593e68c73d8
Robby Decosemaeker <robby.decosemaeker@infosupport.com>
2023-03-22 18:42:17 +0100

    Initial application

---
+++ src/test/java/be/joengenduvel/cookgraph/CookGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package be.joengenduvel.cookgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
